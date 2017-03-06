17cd397bbaf3da146fb41dcf02529b5ab249daf4
vagrant <vagrant@localhost.localdomain>
2017-03-06 08:51:16 +0000

    first commit for workstation cookbook

---
+++ recipes/setup.rb
@@ -0,0 +1,18 @@
+package 'tree' do
+	action :install
+end
+
+package 'ntp' do
+	action :install
+end
+
+package 'git' do
+	action :install
+end
+
+file '/etc/motd' do
+	content 'This server is the property of drlatt'
+	owner 'root'
+	group 'root'
+	action :create
+end
